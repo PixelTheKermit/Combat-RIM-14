da44c36d5fc932ae9b932b60190f8addb79c76ba
Kara <lunarautomaton6@gmail.com>
2022-12-18 09:23:29 -0600

    desaturate chat colors

--- Content.Shared/Chat/ChatChannelExtensions.cs
+++ Content.Shared/Chat/ChatChannelExtensions.cs
@@ -7,11 +7,11 @@ public static class ChatChannelExtensions
         return channel switch
         {
             ChatChannel.Server => Color.Orange,
-            ChatChannel.Radio => Color.LimeGreen,
-            ChatChannel.LOOC => Color.MediumTurquoise,
-            ChatChannel.OOC => Color.LightSkyBlue,
-            ChatChannel.Dead => Color.MediumPurple,
-            ChatChannel.Admin => Color.Red,
+            ChatChannel.Radio => Color.FromHex("#32861A"),
+            ChatChannel.LOOC => Color.FromHex("#429E9B"),
+            ChatChannel.OOC => Color.FromHex("#709FBD"),
+            ChatChannel.Dead => Color.FromHex("#715AA1"),
+            ChatChannel.Admin => Color.FromHex("#B30808"),
             ChatChannel.Whisper => Color.DarkGray,
             _ => Color.LightGray
         };
