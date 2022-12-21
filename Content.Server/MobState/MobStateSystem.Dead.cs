408e9fc0e87c98d60e3c326dfb375a3c21063708
PixelTheKermit <85175107+PixelTheKermit@users.noreply.github.com>
2022-12-21 22:37:54 +0000

    death message
    Signed-off-by: PixelTheKermit <85175107+PixelTheKermit@users.noreply.github.com>

--- Content.Server/MobState/MobStateSystem.Dead.cs
+++ Content.Server/MobState/MobStateSystem.Dead.cs
@@ -1,13 +1,21 @@
 using Content.Shared.Alert;
+using Content.Shared.MobState.Components;
+using Content.Shared.Popups;
 using Content.Shared.StatusEffect;
 
 namespace Content.Server.MobState;
 
 public sealed partial class MobStateSystem
 {
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+
     public override void EnterDeadState(EntityUid uid)
     {
         base.EnterDeadState(uid);
+
+        var component = Comp<MobStateComponent>(uid);
+
+        _popupSystem.PopupEntity(Loc.GetString(component.DeathMessage, ("mob", Comp<MetaDataComponent>(uid).EntityName)), uid, PopupType.SmallCaution);
 
         Alerts.ShowAlert(uid, AlertType.HumanDead);
 
