346d5743086dc2ae8e9b3e8826c905edcef66741
PixelTheKermit <pixelmontrainer8967@gmail.com>
2023-04-04 00:06:38 +0100

    flex your riches once you escape or something idk

--- Content.Server/_CombatRim/Misc/EscapeRuleSystem.cs
+++ Content.Server/_CombatRim/Misc/EscapeRuleSystem.cs
@@ -1,9 +1,18 @@
 
+using System.Linq;
 using Content.Server.GameTicking;
 using Content.Server.GameTicking.Rules;
 using Content.Server.Ghost.Components;
 using Content.Server.Mind;
+using Content.Server.Stack;
+using Content.Server.Store.Components;
+using Content.Shared.Containers;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Stacks;
+using Robust.Server.Containers;
+using Robust.Shared.Configuration;
+using Robust.Shared.Containers;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server._CombatRim.Rules
 {
@@ -14,6 +23,10 @@ namespace Content.Server._CombatRim.Rules
         [Dependency] private readonly MindTrackerSystem _mindTracker = default!;
         [Dependency] private readonly GameTicker _gameTicker = default!;
         [Dependency] private readonly MobStateSystem _mobStateSys = default!;
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
+        [Dependency] private readonly ContainerSystem _containerSystem = default!;
+        [Dependency] private readonly StackSystem _stackSystem = default!;
+        [Dependency] private readonly EntityManager _entityManager = default!;
         public override void Initialize()
         {
             base.Initialize();
@@ -23,7 +36,7 @@ namespace Content.Server._CombatRim.Rules
 
         private void OnRoundEndText(RoundEndTextAppendEvent args)
         {
-            List<(string, string)> escapees = new();
+            List<(string, string, int)> escapees = new();
 
             foreach (var mind in _mindTracker.AllMinds)
             {
@@ -32,12 +45,27 @@ namespace Content.Server._CombatRim.Rules
                 if (uid == null || !TryComp<TransformComponent>(uid, out var xform) || HasComp<GhostComponent>(uid))
                     continue;
 
-                if (!_mobStateSys.IsDead(uid.Value) && xform.MapID != _gameTicker.DefaultMap) // This means you've escaped! good job!
+                if (!_mobStateSys.IsDead(uid.Value) && xform.MapID != _gameTicker.DefaultMap && // This means you've escaped! good job!
+                    mind.TryGetSession(out var session) && mind.CharacterName != null) // Gotta have a session though!
                 {
-                    if (mind.TryGetSession(out var session) && mind.CharacterName != null) // Gotta have a session though!
+                    List<EntityUid> storedUids = new();
+
+                    var totalCash = 0;
+
+                    RecursiveAdd(uid.Value, storedUids);
+
+                    foreach (var ent in storedUids)
                     {
-                        escapees.Add((mind.CharacterName, session.Name));
+                        if (TryComp<CurrencyComponent>(ent, out var curComp) && curComp.Price.TryGetValue(_cfg.GetCVar<string>(CombatRimCVars.MainCurrency), out var cash))
+                        {
+                            if (TryComp<StackComponent>(ent, out var stackComp))
+                                totalCash += (int) cash*stackComp.Count;
+                            else
+                                totalCash += (int) cash;
+                        }
                     }
+
+                    escapees.Add((mind.CharacterName, session.Name, totalCash));
                 }
             }
 
@@ -47,16 +75,31 @@ namespace Content.Server._CombatRim.Rules
                 return;
             }
 
-            if (escapees.Count == 1)
+            if (escapees.Count == 1) // One escapee
                 args.AddLine(Loc.GetString("cr-end-round-lone-escapee"));
-            else
+            else // Multiple escapees
                 args.AddLine(Loc.GetString("cr-end-round-escapees-count", ("count", escapees.Count)));
 
-            foreach (var (name, user) in escapees)
+            foreach (var (name, user, cash) in escapees)
             {
-                args.AddLine(Loc.GetString("cr-end-round-escapee", ("name", name), ("user", user))); // TODO: Display how many spacebucks you made it out with
+                args.AddLine(Loc.GetString("cr-end-round-escapee", ("name", name), ("user", user), ("cash", cash))); // TODO: Display how many spacebucks you made it out with
             }
             args.AddLine(Loc.GetString("cr-end-round-postscript"));
+        }
+
+        private void RecursiveAdd(EntityUid uid, List<EntityUid> toAdd)
+        {
+            if (!HasComp<ContainerManagerComponent>(uid))
+                return;
+
+            foreach (var container in _containerSystem.GetAllContainers(uid))
+            {
+                foreach (var ent in container.ContainedEntities)
+                {
+                    toAdd.Add(ent);
+                    RecursiveAdd(ent, toAdd);
+                }
+            }
         }
 
         public override void Started() { }
